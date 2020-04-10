906e7b8a276bb5004198214b9ac2a3a5b024b900
xu <798286730@qq.com>
2020-04-10 22:43:24 +0800

    leetcode 0410

---
+++ java/src/leetcode/_151/Solution.java
@@ -0,0 +1,31 @@
+package leetcode._151;
+/*
+ *Created by XuFan
+ *on 2020/4/10
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static String reverseWords(String s) {
+        String [] arr = s.trim().split("\\s+");
+        StringBuilder stringBuilder =new StringBuilder();
+        for (int i = arr.length-1; 0<=i ; i--) {
+
+                if (i==0){
+                    stringBuilder.append(arr[i]);
+                }else {
+                    stringBuilder.append(arr[i]).append(" ");
+                }
+
+
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords(" hello          world!          ")+"'");
+    }
+}
